c48a654132886f1f75262f3b34b33150823e480a
huskier <huskier@gmail.com>
2024-03-24 23:40:57 +0800

    1. small updates......

--- tests/AxisConstraints.py
+++ tests/AxisConstraints.py
@@ -1,18 +1,13 @@
 import cadquery as cq
-
 from cadquery.vis import show
 
 cone = cq.Solid.makeCone(1, 0, 2)
 
 assy = cq.Assembly()
 assy.add(cone, name="cone0", color=cq.Color("green"))
-#show(assy)
 assy.add(cone, name="cone1", color=cq.Color("blue"))
-#show(assy)
 assy.constrain("cone0@faces@<Z", "cone1@faces@<Z", "Axis")
 
 assy.solve()
-
 #assy.save('assy.step')
-
 show(assy)
