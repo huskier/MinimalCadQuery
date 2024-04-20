349040c2a186362a97329482c6ee45bc5e41e7cf
huskier <zhangyoubao@siom.ac.cn>
2024-04-20 20:06:03 +0800

    Update MinimalTest.py
    small changes......

--- tests/MinimalTest.py
+++ tests/MinimalTest.py
@@ -1,5 +1,4 @@
 import minimalcadquery as cq
-import sys
 
 rectBasedObject = cq.Workplane().rect(10, 20).extrude(50)
 cq.exporters.export(rectBasedObject, "rectBasedObject.step", exportType = "STEP")
